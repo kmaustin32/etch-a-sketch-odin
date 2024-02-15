fd6696582709ffc0dbf14dd6c25fe93049d32ac9
Keenan (Ezra) Austin <kmaustin32@gmail.com>
2024-02-15 18:34:02 +0200

    Add function for tile creation and mouseover effects

--- scripts.js
+++ scripts.js
@@ -6,8 +6,43 @@ btnGrid.addEventListener('click', (e) => {
   while (isNaN(tileNum) || tileNum > 60 || tileNum < 10) {
     tileNum = prompt('Please pick a number between 10 and 60.')
     tileNum = parseInt(tileNum);
-
     console.log(`final num: ${tileNum}`)
+    
+    removeGrid();
+    initializeGrid(tileNum);
   };
+  tileNum = 0;
 });
 
+const initializeGrid = (gridSize) => {
+  let docFrag = new DocumentFragment();
+
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement('div');
+
+    for (let j = 0; j < gridSize; j++) {
+      let tile = document.createElement('div');
+      tile.classList.add('emptyCell');
+      
+      tile.addEventListener('mouseover', (e) => {
+        tile.classList.toggle('coloredCell');
+        tile.classList.toggle('emptyCell');
+      });
+
+      tile.textContent = j + 1;
+      row.appendChild(tile);
+    };
+
+    docFrag.appendChild(row);
+  };
+
+  container.appendChild(docFrag);
+};
+
+const removeGrid = () => {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  };
+};
+
+initializeGrid(16);
